551ef8c7f8a12065c1cb30063555c9b39b25bb0d
Diniru Sandipa <it20041120@my.sliit.lk>
2021-09-08 01:56:00 +0530

    app.js init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Homeheader from './components/Homeheader';
+import Homefooter from './components/Homefooter';
+
+
+export default class App extends Component {
+  render() {
+    return (
+    <BrowserRouter>
+      <div>
+
+      <Route path=""  component={Homeheader}></Route>
+        
+
+
+
+
+
+
+        
+        <Route path="" component={Homefooter}></Route>
+
+      </div>
+    </BrowserRouter>
+    )
+  }
 }
-
-export default App;
