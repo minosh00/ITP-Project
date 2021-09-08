5ce3d41507f3c9d361ac73867d7e668c13fb6b32
Hivindu Punsith <86920745+Hivindu-Punsith@users.noreply.github.com>
2021-09-08 15:15:11 +0530

    HiviBackend

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const app = express();
 
 //import routes here
-
-
+const applicationRoutes = require('./routes/applications')
+const registeredRoutes = require('./routes/registered')
 
 
 
@@ -20,12 +20,13 @@ app.use(cors());
 
 
 //route middleware
+app.use(applicationRoutes);
+app.use(registeredRoutes);
 
 
 
 
-
-const PORT = 8080;
+const PORT = 8070;
 const DB_URL = 'mongodb+srv://admin:admin@institutedb.nhxwx.mongodb.net/instituteDB?retryWrites=true&w=majority';
 
 mongoose.connect(DB_URL, {
