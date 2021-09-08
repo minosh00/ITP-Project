4eb5bde9e5d4a45a428e97ea22bad18f57d5f0aa
kushan00 <kushandimantha533@gmail.com>
2021-09-09 02:30:10 +0530

    malki edit schange

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -146,7 +146,7 @@ import StdPdf from './components/StdPdf';
 
         <Route path="/AdminStudnethome"  component={Malkimainhome}></Route>
         <Route path="/add" component={CreatePost}></Route>
-        <Route path="/edit/:id" component={EditPost}></Route>
+        <Route path="/stdedit/:id" component={EditPost}></Route>
         <Route path="/post/:id" component={PostDetails}></Route>
         <Route path="/studentapplications" component={Home}></Route>
         <Route path="/addstudent/:id" component={addstudent}></Route>
