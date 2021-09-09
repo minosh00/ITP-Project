27f8ff1e12929b534752975d173a4416be333086
minosh <it20088682@my.sliit.lk>
2021-09-09 10:26:18 +0530

    minosh_backend

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -10,7 +10,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
 
     destination:(req, file,callback) =>{
-     callback(null, "../frontend/public/uploads/");
+     callback(null, "./frontend/public/uploads/");
 
 
     },
