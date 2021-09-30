3987dd0a33412d2b7c12f094de4edabe00500b60
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2021-10-01 02:04:08 +0530

    nethmi changes

---
+++ frontend/src/components/detailsHome.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import axios from'axios';
+import './style.css'
+
+
+export default class detailsHome extends Component {
+constructor(props){
+  super(props);
+
+  this.state={
+    updateDetails:[]
+
+  };
+}
+
+componentDidMount(){
+  this.retrieveUpdateDetails();
+}
+
+retrieveUpdateDetails(){
+  axios.get("/lecDetailsReq").then(res =>{
+    if(res.data.success){
+     this.setState({
+        updateDetails:res.data.existingUpdateDetails
+     });
+
+     console.log(this.state.updateDetails);
+    }
+  });
+}
+onDelete = (id) =>{
+  axios.delete(`/detailreq/delete/${id}`).then((then) =>{
+    alert("Delete Successfully!");
+    this.retrieveUpdateDetails();
+  })
+}
+
+filterDate(updateDetails,searchKey){
+
+  const result = updateDetails.filter((detail) =>
+  detail.date.toLowerCase().includes(searchKey)||
+  detail.notice.toLowerCase().includes(searchKey)
+  
+  
+  )
+
+  this.setState({updateDetails:result})
+}
+
+handleSearchArea = (e) =>{
+
+  const searchKey = e.currentTarget.value;
+axios.get("/lecDetailsReq").then(res =>{     
+      if(res.data.success){
+
+        this.filterDate(res.data.existingUpdateDetails,searchKey)
+      }
+    });
+}
+
+
+
+  render() {
+    return(
+      
+      <div className="#" >
+        <div className="row">
+          <div className="col-lg-12 mt-3 mb-3">
+           
+           
+          
+            <div  className ="#">
+            <h1><center><b>Special Notices For Lecturers</b></center></h1>
+            </div>
+            </div>
+            <div className="col-lg-3 mt-2 mb-2">
+         
+              
+              </div>
+              </div>
+
+
+        <table className =" table table-bordered" >
+       
+      
+        
+        
+          {this.state.updateDetails.map((updateDetail,index) =>(
+             <div key ={index} className="card mb-3">
+               <div className="cardstyle">
+                <th scope = "row">{index+1}</th>
+                
+                <h5  style={{ marginRight: '1700px' }}>Date :{updateDetail.date}</h5>
+                <h5 style={{ color: 'red'  }}>{updateDetail.notice}</h5>
+                
+                    
+               
+                
+                </div>
+             </div>
+
+
+          ))}
+
+        
+        </table>
+     
+       
+        
+    
+      </div>
+      
+    )
+  }
+}
