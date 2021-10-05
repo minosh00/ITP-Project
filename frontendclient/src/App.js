098e03ef0d5aa04fc16b0788daa7a66b73f5aa07
minosh <it20088682@my.sliit.lk>
2021-10-05 22:45:06 +0530

    minosh

--- frontendclient/src/App.js
+++ frontendclient/src/App.js
@@ -24,6 +24,7 @@ import Displaytimetables from './components/Displaytimetables';
 // minosh
 import Courses from './components/Courses';
 import Course from './components/Course';
+import BuyCourseDula from './components/BuyCourseDula';
 
 
 
@@ -75,6 +76,7 @@ import profile from './components/profile';
         {/* minosh */}
         <Route path = "/buycourse" component = {BuyCourseDula}></Route>
         <Route  path="/course"  render={()=> <Courses posts={posts} />} />
+        <Route path="/course1/:id"    render={(props)=>  <Course {...props} posts={posts} />} />
 
         
 
